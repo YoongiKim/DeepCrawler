047157d9e4699b92719e63bc67af7981ec865245
litcoderr <litcoderr@gmail.com>
2018-11-24 17:19:09 +0900

    sampler gui setup

--- DeepCrawler/Sampler/main.py
+++ DeepCrawler/Sampler/main.py
@@ -1,7 +1,19 @@
 from Crawler import CRAWLER
+from .samplerGUI import SAMPLERGUI
+
+from tkinter import Tk
 
 class SAMPLER():
     def __init__(self):
         # Initialize Sampler
         self.crawler = CRAWLER(['NaverBlog','Google'],['햄버거래시피','샐러드래시피'],10,5)
         self.crawler.run()
+
+        # Start GUI
+        self.startGUI()
+
+    def startGUI(self):
+        root = Tk()
+        gui = SAMPLERGUI(root)
+        root.mainloop()
+
---
+++ DeepCrawler/Sampler/samplerGUI.py
@@ -0,0 +1,18 @@
+from tkinter import Label,Button
+
+class SAMPLERGUI():
+    def __init__(self, master):
+        self.master = master
+        master.title("A simple GUI")
+
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.pack()
+
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
+
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
