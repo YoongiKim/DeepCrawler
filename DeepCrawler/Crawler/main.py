22b7da47667b0f17a7e534ce35f422185a2cf340
litcoderr <litcoderr@gmail.com>
2018-11-24 14:31:47 +0900

    added multiprocessing function to start crawling

--- DeepCrawler/Crawler/main.py
+++ DeepCrawler/Crawler/main.py
@@ -1,27 +1,38 @@
 from Crawler import collectData
+from multiprocessing import Pool
 
 class CRAWLER():
-    def __init__(self,platform,query,num):
+    def __init__(self,platform,query,n_data,n_thread=4):
         # Constant Variables
         # TODO Change this when adding platform
         self.validplatforms = ['NaverBlog','Google']
         # Setup Variables
         self.platform = platform # list of platforms
         self.query = query # list of query to search
-        self.num = num # number of datasets(pages) to get
+        self.n_data = n_data # number of datasets(pages) to get
+        self.n_thread = n_thread # number of threads
 
         # Platform validation check
         assert self.platformValidity_check(self.platform), 'Platform list contains invalid platform names'
+                
+        # Store crawled data (number of platform,number of data for each platform)
+        self.result = {}
+        for p in self.platform:
+            self.result[p] = []
+
+    def download(self,task):
+        # TODO download process
+        print(task)
 
     # Main Run Method to activate crawler. Call this to Start Crawling
     def run(self):
-        # Store crawled data (number of platform,number of data for each platform)
-        result = {}
-        for p in self.platform:
-            result[p] = []
-
-        # Start Crawling
-        print('Query:{}||Platform:{}||NumberofData:{}'.format(self.query,self.platform,self.num))
+        print('Query:{}||Platform:{}||data_num:{}||thread_num:{}'.format(self.query,self.platform,self.n_data,self.n_thread))
+        # Start Crawling with MultiProcess
+        pool = Pool(self.n_thread)
+        # TODO pass proper iterable task
+        pool.map_async(self.download,[1,2,3,4])
+        pool.close()
+        pool.join()
 
     # Return if platform list is valid or not
     def platformValidity_check(self,platform):
--- DeepCrawler/Sampler/main.py
+++ DeepCrawler/Sampler/main.py
@@ -3,5 +3,5 @@ from Crawler import CRAWLER
 class SAMPLER():
     def __init__(self):
         # Initialize Sampler
-        self.crawler = CRAWLER(['NaverBlog','Google'],['햄버거래시피','샐러드래시피'],3)
+        self.crawler = CRAWLER(['NaverBlog','Google'],['햄버거래시피','샐러드래시피'],10,5)
         self.crawler.run()
