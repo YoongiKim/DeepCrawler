dccbed55c35197ca0080f338168c7555d7e18c1f
litcoderr <litcoderr@gmail.com>
2018-11-25 00:05:03 +0900

    gui setup

--- DeepCrawler/Sampler/main.py
+++ DeepCrawler/Sampler/main.py
@@ -6,8 +6,8 @@ from tkinter import Tk
 class SAMPLER():
     def __init__(self):
         # Initialize Sampler
-        self.crawler = CRAWLER(['NaverBlog','Google'],['햄버거래시피','샐러드래시피'],10,5)
-        self.crawler.run()
+        #self.crawler = CRAWLER(['NaverBlog','Google'],['햄버거래시피','샐러드래시피'],10,5)
+        #self.crawler.run()
 
         # Start GUI
         self.startGUI()
--- DeepCrawler/Sampler/samplerGUI.py
+++ DeepCrawler/Sampler/samplerGUI.py
@@ -8,8 +8,10 @@ class SAMPLERGUI():
         master.resizable(0,0)
         master.grid_columnconfigure(0, weight=1)
 
-        self.setup_button = Button(master, width=50,height=2,text="Setup", command=master.quit).grid(row=0,column=0, sticky='nw')
-        self.sample_button = Button(master, width=50,height=2,text="Sample", command=master.quit).grid(row=0,column=1, sticky='nw')
+        self.setup_button = Button(master, width=50,height=2,text="Setup", command=master.quit)
+        self.setup_button.grid(row=0,column=0, sticky='nw')
+        self.sample_button = Button(master, width=50,height=2,text="Sample", command=master.quit)
+        self.sample_button.grid(row=0,column=1, sticky='nw')
 
     def greet(self):
         print("Greetings!")
