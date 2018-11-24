84621dc8c6aa685f8d7be0b01e30068379042a8e
litcoderr <litcoderr@gmail.com>
2018-11-24 12:49:26 +0900

    intialization process of crawler made

--- DeepCrawler/Sampler/main.py
+++ DeepCrawler/Sampler/main.py
@@ -1,3 +1,7 @@
+from Crawler import CRAWLER
+
 class SAMPLER():
     def __init__(self):
-        print('Initialize Sampler')
+        # Initialize Sampler
+        self.crawler = CRAWLER(['NaverBlog'],['래시피'],3)
+        self.crawler.run()
