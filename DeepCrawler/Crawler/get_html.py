eb6fce2339a347682217e9d2dee2c2b861058a23
Yoongi Kim <naver7731@gmail.com>
2018-11-24 18:43:06 +0900

    get_html1

--- DeepCrawler/Crawler/get_html.py
+++ DeepCrawler/Crawler/get_html.py
@@ -5,54 +5,73 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementNotVisibleException
 from urllib.parse import quote
 from Crawler.platforms import Platforms
+import platform
+import os
+import sys
 
-def get_post_links(platform, keyword, page_num):
-    browser = webdriver.Chrome()
+class GetHTML:
+    def __init__(self):
+        executable = ''
 
-    keyword_encode = quote(keyword)
+        if platform.system() == 'Windows':
+            print('Detected OS : Windows')
+            executable = './chromedriver/chromedriver_win.exe'
+        elif platform.system() == 'Linux':
+            print('Detected OS : Linux')
+            executable = './chromedriver/chromedriver_linux'
+        elif platform.system() == 'Darwin':
+            print('Detected OS : Darwin')
+            executable = './chromedriver/chromedriver_mac'
+        else:
+            assert False, 'Unknown OS Type'
 
-    if platform == Platforms.GOOGLE:
-        pass
+        print(executable)
+        self.browser = webdriver.Chrome(executable)
 
-    elif platform == Platforms.NAVER_BLOG:
-        link = "https://section.blog.naver.com/Search/Post.nhn?pageNo={}&rangeType=ALL&orderBy=sim&keyword={}"\
-            .format(page_num, keyword_encode)
+    def get_post_links(self, platform, keyword, page_num):
+        keyword_encode = quote(keyword)
 
-        browser.get(link)
+        if platform == Platforms.GOOGLE:
+            pass
 
-    time.sleep(1)
+        elif platform == Platforms.NAVER_BLOG:
+            link = "https://section.blog.naver.com/Search/Post.nhn?pageNo={}&rangeType=ALL&orderBy=sim&keyword={}" \
+                .format(page_num, keyword_encode)
 
-    elem = browser.find_element_by_tag_name("body")
+            self.browser.get(link)
 
-    posts = browser.find_elements(By.XPATH, '//a[@class="desc_inner"]')
+        time.sleep(1)
 
-    links = []
+        elem = self.browser.find_element_by_tag_name("body")
 
-    for post in posts:
-        links.append(post.get_attribute("href"))
+        posts = self.browser.find_elements(By.XPATH, '//a[@class="desc_inner"]')
 
-    browser.close()
+        links = []
 
-    return links
+        for post in posts:
+            links.append(post.get_attribute("href"))
 
-def get_html(platform, post_link):
-    browser = webdriver.Chrome()
-    browser.get(post_link)
-    time.sleep(1)
+        return links
 
-    elem = browser.find_element(By.XPATH, '//iframe[@id="mainFrame"]')
+    def get_html(self, platform, post_link):
+        self.browser.get(post_link)
+        time.sleep(1)
 
-    if platform == Platforms.GOOGLE:
-        pass
+        if platform == Platforms.GOOGLE:
+            pass
 
-    elif platform == Platforms.NAVER_BLOG:
-        for i in range(60):
-            elem.send_keys(Keys.PAGE_DOWN)
-            time.sleep(0.2)
+        elif platform == Platforms.NAVER_BLOG:
+            frame = self.browser.find_element(By.XPATH, '//iframe[@id="mainFrame"]')
 
-    print(elem)
+            for i in range(60):
+                frame.send_keys(Keys.PAGE_DOWN)
+                time.sleep(0.2)
+
+            htmls = self.browser.find_elements_by_tag_name('html')
+            print(htmls[-1].get_attribute('innerHTML'))
 
 if __name__ == '__main__':
-    links = get_post_links(Platforms.NAVER_BLOG, '김밥', 1)
+    get_html = GetHTML()
+    links = get_html.get_post_links(Platforms.NAVER_BLOG, '김밥', 1)
 
-    get_html(Platforms.NAVER_BLOG, links[0])
+    get_html.get_html(Platforms.NAVER_BLOG, links[0])
