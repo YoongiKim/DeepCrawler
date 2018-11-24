150ca607288d960e477c447170f7135c897f117a
Yoongi Kim <naver7731@gmail.com>
2018-11-24 09:11:06 +0900

    chrome driver

--- DeepCrawler/Crawler/get_html.py
+++ DeepCrawler/Crawler/get_html.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementNotVisibleException
 from urllib.parse import quote
-from DeepCrawler.Crawler.platforms import Platforms
+from Crawler.platforms import Platforms
 
 def get_post_links(platform, keyword, page_num):
     browser = webdriver.Chrome()
@@ -26,12 +26,33 @@ def get_post_links(platform, keyword, page_num):
 
     posts = browser.find_elements(By.XPATH, '//a[@class="desc_inner"]')
 
+    links = []
+
     for post in posts:
-        link = post.get_attribute("href")
-        print(link)
+        links.append(post.get_attribute("href"))
 
     browser.close()
 
+    return links
+
+def get_html(platform, post_link):
+    browser = webdriver.Chrome()
+    browser.get(post_link)
+    time.sleep(1)
+
+    elem = browser.find_element(By.XPATH, '//iframe[@id="mainFrame"]')
+
+    if platform == Platforms.GOOGLE:
+        pass
+
+    elif platform == Platforms.NAVER_BLOG:
+        for i in range(60):
+            elem.send_keys(Keys.PAGE_DOWN)
+            time.sleep(0.2)
+
+    print(elem)
 
 if __name__ == '__main__':
-    get_html(Platforms.NAVER_BLOG, '김밥', 1)
+    links = get_post_links(Platforms.NAVER_BLOG, '김밥', 1)
+
+    get_html(Platforms.NAVER_BLOG, links[0])
