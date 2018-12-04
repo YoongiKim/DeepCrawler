cabc23ed2f1c4a289ee29e891b7cba021e865832
litcoderr <litcoderr@gmail.com>
2018-12-04 20:31:06 +0900

    working on gui

--- DeepCrawler/Sampler/samplerGUI.py
+++ DeepCrawler/Sampler/samplerGUI.py
@@ -5,23 +5,25 @@ class SAMPLERGUI():
     def __init__(self, master):
         self.master = master
         master.title("Sampler (DeepCrawler)")
-        master.geometry("1000x700")
         master.resizable(0,0)
 
         # Initialize Frame
-        buttonFrame = Frame(self.master,side='top').pack()
-        SetupFrame = Frame(self.master,side='bottom').pack()
-        SampleFrame = Frame(self.master,side='bottom').pack()
+        self.menuFrame = Frame(self.master)
+        self.menuFrame.pack(side='top')
+        self.SetupFrame = Frame(self.master)
+        self.SetupFrame.pack(side='bottom')
+        self.SampleFrame = Frame(self.master)
+        self.SampleFrame.pack(side='bottom')
 
         # Button Frame
-        self.setup_button = Button(buttonFrame, width=50,height=2,text="Setup", command=master.quit)
-        self.setup_button.pack(side = 'left')
-        self.sample_button = Button(buttonFrame, width=50,height=2,text="Sample", command=master.quit)
-        self.sample_button.pack(side = 'right')
+        self.setup_button = Button(self.menuFrame, width=50,height=2,text="Setup", command=self.master.quit)
+        self.setup_button.pack(side='left')
+        self.sample_button = Button(self.menuFrame, width=50,height=2,text="Sample", command=self.master.quit)
+        self.sample_button.pack(side='left')
 
         # Setup Frame
         # TODO Make Setup Frame and make crawler calling method
-        
+
 
         # Sample Frame
         # TODO Make Sample Frame and make sampling and saving method
