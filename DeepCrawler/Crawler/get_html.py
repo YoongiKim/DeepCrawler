509d307baf75f61f00d1167fd6e514768c15e785
litcoderr <litcoderr@gmail.com>
2018-12-07 15:46:06 +0900

    implemented crawling process to GUI App

--- DeepCrawler/Crawler/get_html.py
+++ DeepCrawler/Crawler/get_html.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import ElementNotVisibleException
 from urllib.parse import quote
-from Crawler.platforms import Platforms
+from .platforms import Platforms
 import platform
 import os
 import sys
@@ -12,16 +12,15 @@ import sys
 class GetHTML:
     def __init__(self):
         executable = ''
-
         if platform.system() == 'Windows':
             print('Detected OS : Windows')
-            executable = './chromedriver/chromedriver_win.exe'
+            executable = './DeepCrawler/Crawler/chromedriver/chromedriver_win.exe'
         elif platform.system() == 'Linux':
             print('Detected OS : Linux')
-            executable = './chromedriver/chromedriver_linux'
+            executable = './DeepCrawler/Crawler/chromedriver/chromedriver_linux'
         elif platform.system() == 'Darwin':
             print('Detected OS : Darwin')
-            executable = './chromedriver/chromedriver_mac'
+            executable = './DeepCrawler/Crawler/chromedriver/chromedriver_mac'
         else:
             assert False, 'Unknown OS Type'
 
@@ -33,7 +32,6 @@ class GetHTML:
 
         if platform == Platforms.GOOGLE:
             pass
-
         elif platform == Platforms.NAVER_BLOG:
             link = "https://section.blog.naver.com/Search/Post.nhn?pageNo={}&rangeType=ALL&orderBy=sim&keyword={}" \
                 .format(page_num, keyword_encode)
@@ -43,11 +41,9 @@ class GetHTML:
         time.sleep(1)
 
         elem = self.browser.find_element_by_tag_name("body")
-
         posts = self.browser.find_elements(By.XPATH, '//a[@class="desc_inner"]')
 
         links = []
-
         for post in posts:
             links.append(post.get_attribute("href"))
 
@@ -56,10 +52,8 @@ class GetHTML:
     def get_html(self, platform, post_link):
         self.browser.get(post_link)
         time.sleep(1)
-
         if platform == Platforms.GOOGLE:
-            pass
-
+            return "Google Not Implemented Yet"
         elif platform == Platforms.NAVER_BLOG:
             frame = self.browser.find_element(By.XPATH, '//iframe[@id="mainFrame"]')
 
@@ -67,11 +61,6 @@ class GetHTML:
                 frame.send_keys(Keys.PAGE_DOWN)
                 time.sleep(0.2)
 
-            htmls = self.browser.find_elements_by_tag_name('html')
-            print(htmls[-1].get_attribute('innerHTML'))
-
-if __name__ == '__main__':
-    get_html = GetHTML()
-    links = get_html.get_post_links(Platforms.NAVER_BLOG, '김밥', 1)
-
-    get_html.get_html(Platforms.NAVER_BLOG, links[0])
+            html = self.browser.find_elements_by_tag_name('html')
+            html = html[-1].get_attribute('innerHTML')
+            return html
